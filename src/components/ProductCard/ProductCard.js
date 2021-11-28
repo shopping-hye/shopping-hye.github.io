5f7150a51e667e99c7fa208dafe28df1f79a7154
SLYJason <jasongluyi@gmail.com>
2021-11-27 23:45:19 -0500

    first commit

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+export class ProductCard extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	handleMouseMove = event => {
+		if (event.type === 'mouseenter') {
+			console.log(`you moved in ${name} at: ${new Date().toLocaleTimeString()}`)
+		}
+		if (event.type === 'mouseleave') {
+			console.log(`you moved out ${name} at: ${new Date().toLocaleTimeString()}`)
+		}
+	}
+
+	render() {
+		const { name, imageUri, description } = this.props;
+		return (
+			<div onMouseEnter={this.handleMouseMove}
+					 onMouseLeave={this.handleMouseMove}>
+				<Card
+					hoverable
+					cover={<img alt="example" src={imageUri} />}
+				>
+					<Meta title={name} description={description} />
+				</Card>
+			</div>
+		)
+	}
+}
