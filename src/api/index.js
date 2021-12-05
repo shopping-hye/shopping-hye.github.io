6e06d1b3425f59e0fc2a16e9237ed80384b896a2
SLYJason <jasongluyi@gmail.com>
2021-12-05 17:50:31 -0500

    short uuid

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 const endpoint = 'https://j3uvhhijq0.execute-api.us-east-1.amazonaws.com/api/hye';
-import { v4 as uuid4 } from 'uuid';
 
 export async function post(data) {
 	let config = {
@@ -10,7 +9,7 @@ export async function post(data) {
 	}
 
 	const article = {
-		user_id: uuid4(),
+		user_id: generateUID(),
 		checkout_time: new Date().toLocaleString(),
 		data: data
 	};
@@ -19,4 +18,14 @@ export async function post(data) {
 	}).catch(err => {
 		console.error('failed to save response: ', err);
 	})
+}
+
+function generateUID() {
+	// I generate the UID from two parts here
+	// to ensure the random number provide enough bits.
+	let firstPart = (Math.random() * 46656) | 0;
+	let secondPart = (Math.random() * 46656) | 0;
+	firstPart = ("000" + firstPart.toString(36)).slice(-3);
+	secondPart = ("000" + secondPart.toString(36)).slice(-3);
+	return firstPart + secondPart;
 }
