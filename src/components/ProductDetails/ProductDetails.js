e1310c9553f98ab608cd956be7a2e733a9d59e2a
SLYJason <jasongluyi@gmail.com>
2021-12-07 21:08:43 -0500

    display success modal

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './ProductDetails.css';
-import { Button, Image, Space, Row, Col } from 'antd';
+import { Button, Image, Space, Row, Col, Modal } from 'antd';
 import { post } from '../../api/api';
 import { getPriceImageUri, getProductImageUri } from "../utils";
 import { products as productsConfig } from '../../product_config.json';
 
 export class ProductDetails extends React.Component {
-	checkout= () => {
-		const { data, products, prices } = this.props;
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalVisible: false,
+			okText: 'Copy Testing ID'
+		}
+	}
 
+	checkout= () => {
+		this.setState({
+			modalVisible: true
+		});
+
+		const { data, products, prices } = this.props;
 		// save to api
 		console.log(`Submitting the data: ${JSON.stringify(data, null, 2)}`);
 		post(data, true).then(res => {}).catch(err => {
 			console.log('failed to save the data')
 		});
+	}
+
+	copyTestingId() {
+		let na = navigator.clipboard.writeText(this.props.data.version + this.props.data.user_id)
+			.then(res => console.log(`${this.props.data.version + this.props.data.user_id} copied`))
+			.catch(err => console.log(err))
+
+		this.setState({
+			okText: 'Copied!'
+		})
 	}
 
 	render() {
@@ -50,6 +71,17 @@ export class ProductDetails extends React.Component {
 							<Button size="large" type="default" onClick={this.props.backToMainPage.bind(this)}>Back</Button>
 						</a>
 						<Button size="large" type="primary" onClick={this.checkout}>Checkout</Button>
+						<Modal
+							title="Your response have been saved!"
+							centered
+							visible={this.state.modalVisible}
+							onOk={() => this.copyTestingId()}
+							closable={false}
+							cancelButtonProps={{disabled: true}}
+							okText={this.state.okText}
+						>
+							<p>Click the button to copy your testing ID and enter it back in the Qualtrics. Your testing ID is: <strong>{data.version + data.user_id}</strong></p>
+						</Modal>
 					</Space>
 				</Col>
 			</Row>
